693ce730e9ae69b24dc5e4867537e6309eac3c65
JonJaryongLee <director9303@gmail.com>
2021-10-12 18:02:57 +0900

    211012

---
+++ 수업/8. JavaScript (4)/코드/예제1/ex1-1.js
@@ -0,0 +1,7 @@
+const menus = ["짜장면", "짬뽕", "탕수육"];
+let i = 0;
+while (menus[i]) {
+  console.log(menus[i]);
+  i++;
+}
+
---
+++ 수업/8. JavaScript (4)/코드/예제1/ex1-2.js
@@ -0,0 +1,4 @@
+const menus = ["짜장면", "짬뽕", "탕수육"];
+for (let i = 0; i < menus.length; i++) {
+  console.log(menus[i]);
+}
